953a689e12d1d5be05fb89918b94cf6a38b56c4a
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-04-13 01:48:40 +0300

    Add RightSidebar and Bottombar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       >
         <ClerkProvider>
           <Topbar />
-          <main>
+          <main className="flex flex-row">
             <LeftSidebar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
