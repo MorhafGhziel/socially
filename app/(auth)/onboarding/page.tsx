d33adb505d08c796df1ea8fb8fd782b25edad857
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-04-13 05:25:58 +0300

    feat: Update onboarding page with improved profile image handling and form components

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,29 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs/server";
+
 async function Page() {
+  const user = await currentUser();
+
+  const userData = {
+    id: user?.id || "",
+    objectId: user?.id || "",
+    username: user?.username || "",
+    name: user?.firstName || "",
+    bio: "",
+    image: user?.imageUrl || "",
+  };
+
+  const userInfo = {};
+
   return (
-    <main>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
+      <p className="text-light-2 text-base-regular">
+        Complete your profile now to use Socially
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 }
